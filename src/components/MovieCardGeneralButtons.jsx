779524b7eeff35282506095e7fdf4bfc0988f52e
Georgi <figoto660@gmail.com>
2022-10-02 22:25:33 -0400

    function optimisation

--- src/components/MovieCardGeneralButtons.jsx
+++ src/components/MovieCardGeneralButtons.jsx
@@ -1,28 +1,34 @@
-import React ,{useContext}from "react";
+import React, { useContext } from "react";
 import { GlobalContext } from "./context/GlobalContext";
 
 const MovieCardGeneralButtons = ({ movie, type }) => {
+  const {
+    deleteMovieFromWatchList,
+    moveMovieFromWatchListToWatched,
+    deleteMovieFromWatched,
+    moveMovieFromWatchedToWatchList,
+  } = useContext(GlobalContext);
 
-    const{deleteMovieFromWatchList,moveMovieFromWatchListToWatched,deleteMovieFromWatched,moveMovieFromWatchedToWatchList} = useContext(GlobalContext);
-    const deleteMovie = ()=>{
-        deleteMovieFromWatchList(movie);
-    }
-    const moveToWatched = () =>{
-        moveMovieFromWatchListToWatched(movie)
-    }
-    
   return (
     <div>
       {type === "watchlist" && (
         <div>
-          <button onClick={moveToWatched}>Move to Watched</button>
-          <button onClick={deleteMovie}>Remove from Watchlist</button>
+          <button onClick={() => moveMovieFromWatchListToWatched(movie)}>
+            Move to Watched
+          </button>
+          <button onClick={() => deleteMovieFromWatchList(movie)}>
+            Remove from Watchlist
+          </button>
         </div>
       )}
-       {type === "watched" && (
+      {type === "watched" && (
         <div>
-          <button onClick={()=> moveMovieFromWatchedToWatchList(movie)}>Move back to Watchlist</button>
-          <button onClick={()=> deleteMovieFromWatched(movie)}>Delete the Movie</button>
+          <button onClick={() => moveMovieFromWatchedToWatchList(movie)}>
+            Move back to Watchlist
+          </button>
+          <button onClick={() => deleteMovieFromWatched(movie)}>
+            Delete the Movie
+          </button>
         </div>
       )}
     </div>
